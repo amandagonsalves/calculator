48547f2f24843b11308929e8cbb7590ec1695458
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-06-23 19:47:11 -0300

    date and time class

--- CalcController.js
+++ CalcController.js
@@ -1,4 +1,4 @@
-const display = document.querySelector('#display');
+/* const display = document.querySelector('#display');
 
 function timeDate() {
 
@@ -19,5 +19,54 @@ function initialize() {
         timeDate()
     },1000)
 }
-initialize()
+initialize() */
+class CalcController {
+    constructor() {
+        this._timeEl = document.querySelector('#time');
+        this._dateEl = document.querySelector('#date');
+        this._displayCalcEl = document.querySelector('#display');
+        this.currentDate;
+        this._locale = 'pt-BR'
+    }
+
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get currentDate() {
+        return new Date()
+    }
+    set currentDate(value) {
+        this.currentDate.innerHTML = value;
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    initialize() {
+        setInterval(()=> {
+            setDisplayDateTime()
+        }, 1000)
+    }
+
+}
 

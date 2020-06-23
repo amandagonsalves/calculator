d1e0a6d1ecb4bd9968ef80abefb85ddbacdc6a76
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-06-23 19:31:03 -0300

    date and time

--- CalcController.js
+++ CalcController.js
@@ -1,6 +1,23 @@
-class CalcController {
-    constructor() {
-       
-    }
-    
+const display = document.querySelector('#display');
+
+function timeDate() {
+
+    const time = document.querySelector('#time');
+    const now = new Date();
+    const timeEl = now.toLocaleTimeString('pt-BR');
+    time.innerHTML = `<p>${timeEl}</p>`
+
+    const date = document.querySelector('#date');
+    const currentDate = new Date();
+    const dateEl = currentDate.toLocaleDateString('pt-BR');
+    date.innerHTML = `<p>${dateEl}</p>`
+
 }
+function initialize() {
+    timeDate()
+    setInterval(()=> {
+        timeDate()
+    },1000)
+}
+initialize()
+

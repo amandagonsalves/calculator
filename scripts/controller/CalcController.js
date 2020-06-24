36908c9c56e66648d4d9ac53fad3d26b67dbbda3
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-06-24 15:58:58 -0300

    last item

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -103,12 +103,20 @@ class CalcController {
     } */
 
     getResult() {
+
         return eval(this._operation.join(''))
     }
 
     calc() {
 
         let last = '';
+        this._lastOperator = this.getLastItem();
+
+        if(this._operation.length < 3) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem,this._lastOperator,this._lastNumber];
+        }
 
         if (this._operation.length > 3) {
 
@@ -116,9 +124,12 @@ class CalcController {
 
             this._lastNumber = this.getResult();
 
+        } else if(this._operation.length == 3){
+
+            this._lastNumber = this.getLastItem(false);
 
         }
-
+        console.log(this._lastNumber,this._lastOperator)
         let result = this.getResult();
 
         if (last === '%') {
@@ -156,22 +167,16 @@ class CalcController {
             }
 
         }
+        if(!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
         return lastItem;
     }
 
     setLastNumberToDisplay() {
 
-        let lastNumber;
+        let lastNumber = this.getLastItem(false)
 
-        for (let i = this._operation.length - 1; i >= 0; i--) {
-
-            if (!this.isOperator(this._operation[i])) {
-
-                lastNumber = this._operation[i];
-                break;
-
-            }
-        }
         if (!lastNumber) {
             lastNumber === 0
         };
